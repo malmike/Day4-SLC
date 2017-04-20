62d65ebc22ef7abd46c5b9df2fb8d0a3916c9bea
Male Michael <malmike21@outlook.com>
2017-04-20 21:18:10 +0300

    Added comments to the binary search function

--- BinarySearch/app/binary_search.py
+++ BinarySearch/app/binary_search.py
@@ -1,43 +1,59 @@
 class BinarySearch(list):
     def __init__(self, a, b):
+        #Inherit the methods in list
+        super(BinarySearch, self).__init__()
+        #Check that the values entered are integers
         if isinstance(a, int) and isinstance(b, int):
-            self.binary_list =  [x*b for x in range(1,a+1)] 
-            #self.binary_list = [x for x in range(b,(a*b)+1,b)]
-            self.length = len(self.binary_list)
+            #Loop used to generate values that are appended to the class list
+            for x in range(b,(a*b)+1,b):
+                self.append(x)
+            #Get the length of the generated class list
+            self.length = len(self)
         else:
             raise TypeError
-    def __getitem__(self,key):
-        return  self.binary_list[key]  
     def search(self, search_item):
-        if isinstance(search_item, int):
-            return self.binary_search(self.binary_list, search_item)
+        #Check that the search item is an integer
+        if isinstance(search_item, int) and len(self) > 0:
+            #Initialize index to -1 and found to false
+            found, index = False, -1
+            #Assign to first first index value of the list
+            first = 0
+            #Assign to last the last index value of the list
+            last = len(self)-1
+            #Assign 0 to a variable count
+            count = 0
+            #Check that the first value in the list is not the search item
+            if self[first]==search_item:
+                found, index = True, first
+            #Check that the last value in the list is not the search item
+            elif self[last]==search_item:
+                found, index = True, last
+            #Check that the search item is not greater than the last item in the list or 
+            #less than the first item in the list
+            elif search_item > self[last] or search_item < self[first]:
+                found, index = True, -1
+            #Loop through the list until the value is found or 
+            #until the value in first is greater than that in last
+            while first<=last and not found:
+                #Get the index at the midpoint between the index in first and that in last
+                midpoint = (first+last)//2
+                #Check whether the value is at the midpoint
+                if self[midpoint] == search_item:
+                    #Set found to true and index to the value in midpoint 
+                    found,index = True, midpoint
+                else:
+                    #Increment the count by 1 to show the number of times the loop has occured
+                    count += 1
+                    #Put the index of midpoint-1 to the last value if search item is less than
+                    #value at midpoint
+                    if search_item < self[midpoint]:
+                        last = midpoint - 1
+                    #Put the index of midpoint=1 to the last value if search item is greater than
+                    #value at midpoint
+                    else:
+                        first = midpoint + 1
+            #Return count and index at which the search item is, in a dictionary
+            return {'count':count, 'index':index}
         else:
             raise TypeError
-    def binary_search(self, a_list, item):
-        first = 0
-        last = len(a_list)-1
-        found = False
-        midpoint = 0
-        count = 0
-        while first<=last and found is False:
-            count += 1
-            midpoint = (first+last)//2
-            if a_list[midpoint] == item:
-                found = True
-                break
-            else:
-                if item < a_list[midpoint]:
-                    last = midpoint - 1
-                else:
-                    first = midpoint + 1
-        if a_list[midpoint] == item:
-            return {'count':count, 'index':midpoint}
-        else:
-            return {'count':count, 'index': -1}
-
-
-
-
-
-
-
+   
